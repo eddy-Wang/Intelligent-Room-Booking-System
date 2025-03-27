b0441789d130a7c94667537e0fe17591b3ea9cd9
Cc1-Yy <2542884@dundee.ac.uk>
2025-03-27 16:44:40 +0800

    refactor: unify login interface architecture style

--- backend/run.py
+++ backend/run.py
@@ -15,15 +15,17 @@ def run_crawler_periodically():
         time.sleep(3600)
 
 
-def  start_scheduler():
+def start_scheduler():
     scheduler = BackgroundScheduler()
     scheduler.add_job(update_booking_status_to_missed, 'cron', hour=23, minute=59)
 
     scheduler.start()
+
+
 if __name__ == '__main__':
     # only run crawler in the subprocess that is actually running
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         threading.Thread(target=run_crawler_periodically, daemon=True).start()
-        
+
     start_scheduler()
     app.run(host="0.0.0.0", port=8080)
