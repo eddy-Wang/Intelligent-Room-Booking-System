b6618c88204c1b3e22820bd8de880854fb9b68b7
SJHPCJS <2224816355@qq.com>
2025-03-27 10:52:43 +0800

    fix: avatars change

--- frontend/src/views/MyReservation.vue
+++ frontend/src/views/MyReservation.vue
@@ -166,9 +166,10 @@ export default {
       return this.filteredReservations.slice(start, end);
     },
     userAvatar() {
-      return this.user.permission === 'student'
-          ? "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          : "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      return this.user.permission === 'Student'
+
+          ? "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":
+          "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     },
   },
   methods: {
